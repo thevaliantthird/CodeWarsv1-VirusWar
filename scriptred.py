13f37560f9b683b938a1f8d9cccb8c9bc4f02c4e
TanmayGanguli <96480278+TanmayGanguli@users.noreply.github.com>
2021-12-25 19:29:39 +0530

    Update scriptred.py

--- scriptred.py
+++ scriptred.py
@@ -2,8 +2,51 @@ from random import randint
 
 
 def ActRobot(robot):
-        if robot.GetVirus() > 1000:
-                robot.DeployVirus(200)        
+                up = robot.investigate_up()
+                down = robot.investigate_down()
+                left = robot.investigate_left()
+                right =robot.investigate_right()
+                if up == "enemy" and robot.GetVirus() > 1000:
+                        robot.DeployVirus(100)        
+                elif up == "enemy-base":
+                        x,y = robot.GetPosition()
+                        
+
+
+                        msg = "base" + str(x) + str(y-1) 
+                        robot.setSignal(msg)
+                        if robot.GetVirus() > 500:
+                            robot.DeployVirus(500)
+                if down == "enemy" and robot.GetVirus() > 1000:
+                        robot.DeployVirus(100)        
+                elif down == "enemy-base":
+                        x,y = robot.GetPosition()
+                        if x < 10:
+                                msg_X = '0' + str(x)
+                        else:
+                                msg_x = str(x)
+                        if y+1 < 10:
+                                msg_y = '0' + str(y+1)
+                        else:
+                                msg_y = str(y+1)        
+                        msg = "base" + str(x) + str(y+1) 
+                        robot.setSignal(msg)
+                        if robot.GetVirus() > 500:
+                            robot.DeployVirus(500)  
+                if right == "enemy" and robot.GetVirus() > 1000:
+                        robot.DeployVirus(100)        
+                elif up == "enemy-base":
+                        x,y = robot.GetPosition()
+                        msg = "base" + str(x) + str(y-1) 
+                        robot.setSignal(msg)
+                        if robot.GetVirus() > 500:
+                            robot.DeployVirus(500)                     
+
+
+
+                if len(robot.GetCurrentBaseSignal())  >0:
+
+
         return randint(1,4)
 
 
